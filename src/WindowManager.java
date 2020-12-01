5440836553e7b944575c7db77dc68871513d9f2d
Ryszard <s19234@pjwskt.edu.pl>
2020-12-01 13:05:17 +0100

    Class WindowManager.java will be used.

--- src/WindowManager.java
+++ src/WindowManager.java
@@ -1,4 +1,4 @@
-/** It's an empty class for now. I don't know if it will be used or not. I need to think this through. */
+/** Operates on an instance of a Window. */
 public class WindowManager extends Window{
 
 }
