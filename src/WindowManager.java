f3f108808cefe12660d76897964a1a2b3f4614b2
Ryszard <s19234@pjwskt.edu.pl>
2020-12-01 13:12:45 +0100

    Has simple code to start a Window.

--- src/WindowManager.java
+++ src/WindowManager.java
@@ -1,4 +1,16 @@
-/** Operates on an instance of a Window. */
-public class WindowManager extends Window{
+import javax.swing.*;
 
+/** Operates on an instance of a Window. */
+public class WindowManager {
+    private Window window = Window.getInstance;
+    public static WindowManager getInstance = new WindowManager();
+
+    private WindowManager() {}
+    public void run() {
+        window.isResizable = false;
+        window.component = null;
+        window.closeOperation = WindowConstants.EXIT_ON_CLOSE;
+        window.isVisible = true;
+        window.run();
+    }
 }
